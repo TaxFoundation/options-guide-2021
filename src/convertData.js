2ff4ef0a5aae6ae10353c6a8f2c16835cc2f2ade
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-12-16 10:34:42 -0500

    Correctly translate CSV to JSON

--- src/convertData.js
+++ src/convertData.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
+const _ = require('lodash');
 
 fs.readdir(path.join(__dirname, '/data'), (err, files) => {
   if (err) {
     console.error(err);
   } else {
     files.forEach(file => {
-      console.log(path.join(__dirname, '/data', file));
+      let results = [];
+      let data = {};
       fs.createReadStream(path.join(__dirname, '/data', file))
-        .pipe(csv())
+        .pipe(csv({ mapHeaders: ({ header, index }) => _.camelCase(header) }))
         .on('data', row => {
-          console.log(row);
+          results.push(row);
         })
         .on('end', () => {
+          console.log(results);
+          results.forEach(row => {
+            const indicator = _.camelCase(row['indicator']);
+            if (!data[indicator]) data[indicator] = {};
+            data[indicator][_.camelCase(row['subIndicator'])] = parseFloat(
+              String(row['value']).replace('$', ''),
+            );
+            console.log(row, row['Indicator'], indicator);
+          });
           console.log(`Finished ${file}`);
+          console.log(data);
         });
     });
   }
