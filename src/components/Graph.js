0bb87576873eda3ae8823b63716ba7cce48adbfb
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-02-04 11:59:59 -0500

    Positioning adjustments to graph based on data min

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -20,21 +20,21 @@ const LegendContainer = styled.div`
 `;
 
 const Graph = ({ conventional, dynamic }) => {
-  const width = 600;
-  const height = 400;
-  const margin = {
-    bottom: 50,
-    left: 60,
-    right: 20,
-    top: 30,
-  };
-
   const values = [...conventional.map(d => d.value), ...dynamic.map(d => d.value), 0];
   const years = [...conventional.map(d => d.year), ...dynamic.map(d => d.year)];
   const min = Math.min(...values);
   const max = Math.max(...values);
   const yearMin = Math.min(...years);
   const yearMax = Math.max(...years);
+
+  const width = 600;
+  const height = 400;
+  const margin = {
+    bottom: min < 0 ? 20 : 40,
+    left: 70,
+    right: 20,
+    top: min < 0 ? 40 : 20,
+  };
 
   const yScale = scaleLinear({
     domain: [max, min],
@@ -60,6 +60,7 @@ const Graph = ({ conventional, dynamic }) => {
         <Text
           dx={(width - margin.left) / 2 + margin.left}
           dy={0}
+          style={{ fontWeight: 700 }}
           textAnchor="middle"
           verticalAnchor="start"
         >
@@ -67,17 +68,34 @@ const Graph = ({ conventional, dynamic }) => {
         </Text>
         <AxisLeft
           label="Change in GDP (Billions)"
+          labelProps={{
+            style: { fontSize: '0.85rem' },
+            textAnchor: 'middle',
+          }}
           left={margin.left}
           scale={yScale}
           tickFormat={v => (+v < 0 ? `-$${Math.abs(v)}` : `$${+v}`)}
+          tickLabelProps={() => {
+            return { style: { fontSize: '0.7rem', textAnchor: 'end' }, dy: 4, dx: -5 };
+          }}
           top={margin.top}
         ></AxisLeft>
         <Axis
           label="Year"
           labelOffset={5}
+          labelProps={{
+            style: {
+              fontSize: '0.85rem',
+            },
+            textAnchor: 'middle',
+            transform: `translate(0,${min < 0 ? -7 : 7})`,
+          }}
           left={margin.left}
           orientation={max === 0 ? 'top' : 'bottom'}
           scale={xScale}
+          tickLabelProps={() => {
+            return { style: { fontSize: '0.7rem', textAnchor: 'middle' }, dy: min <= 0 ? -3 : 3 };
+          }}
           top={yScale(0) + margin.top}
           tickFormat={v => v}
         ></Axis>
