e5cc04380b8a01d8c444dad607d9633292f814fc
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-02-24 10:43:08 -0500

    Better select styles

--- src/components/OptionSelect.js
+++ src/components/OptionSelect.js
@@ -19,30 +19,56 @@ const Heading = styled.h2`
   margin: 0;
 `;
 
+const SelectContainer = styled.div`
+  background-color: #fff;
+  border-radius: 4px;
+  display: grid;
+  align-items: center;
+  grid-template-areas: 'select';
+
+  &::after {
+    background-color: var(--tf-blue);
+    clip-path: polygon(100% 0%, 0 0%, 50% 100%);
+    content: '';
+    grid-area: select;
+    height: 0.5em;
+    justify-self: end;
+    margin-right: 0.5rem;
+    width: 0.8em;
+  }
+`;
+
 const Select = styled.select`
+  appearance: none;
+  background-color: transparent;
+  cursor: pointer;
+  grid-area: select;
   font-size: 1rem;
   text-align: center;
   width: 100%;
+  z-index: 1;
 `;
 
 const OptionSelect = ({ option, options, setOption }) => {
   return (
     <Container>
       <Heading>Choose a Tax Reform Option</Heading>
-      <Select
-        name="option-select"
-        id="option-select"
-        value={option}
-        onChange={e => setOption(e.target.value)}
-      >
-        <option value="home">Options for Tax Reform Home Page</option>
-        {options.map(option => (
-          <option
-            key={`option-${option.id}`}
-            value={option.id}
-          >{`Option ${option.id}: ${option.title}`}</option>
-        ))}
-      </Select>
+      <SelectContainer>
+        <Select
+          name="option-select"
+          id="option-select"
+          value={option}
+          onChange={e => setOption(e.target.value)}
+        >
+          <option value="home">Options for Tax Reform Home Page</option>
+          {options.map(option => (
+            <option
+              key={`option-${option.id}`}
+              value={option.id}
+            >{`Option ${option.id}: ${option.title}`}</option>
+          ))}
+        </Select>
+      </SelectContainer>
     </Container>
   );
 };
