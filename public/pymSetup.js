b9af8978db870282fd50b15d22e2442bc2434d77
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-04-14 15:24:00 -0400

    Add files for using pym to embed iframe

---
+++ public/pymSetup.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+  const pymParent = new pym.Parent(
+    'options-guide-interactive',
+    'https://options-guide-2021.netlify.app/index.html',
+    {},
+  );
+});
